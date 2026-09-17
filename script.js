45af3d65bcd4b46bcb029c247dc4d4d384adb1c5
moggedbyme7-maker <moggedbyme7@gmail.com>
2026-09-17 10:31:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ const games = [
 {name:"Pokemon Blue", file:"pokemon-blue.html"},
 {name:"Pokemon Yellow", file:"pokemon-yellow.html"},
 {name:"Geometry Dash Lite", file:"gd.html"},
-{name:"Yoshi's Island", file:"yoshis-island.html"}
+{name:"Yoshi's Island", file:"yoshis-island.html"},
+{name:"Plants Vs. Zombies 2", file:"pvz2.html"},
 
 
 ];
